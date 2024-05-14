db9602010947ec7d7de2e191c3e45ea06dd9f804
mel-am <122634403+mel-am@users.noreply.github.com>
2024-05-14 13:29:18 +0100

    Update

--- src/app/QueryData/GetAllQuotes.js
+++ src/app/QueryData/GetAllQuotes.js
@@ -1,18 +1,18 @@
-INSERT INTO quotes (quote, character, title, season, episode) VALUES
-('That''s what she said.', 'Michael Scott', 'Sexual Harassment', 2, 2),
-('I''m not superstitious, but I am a little stitious.', 'Michael Scott', 'Fun Run', 4, 1),
-('Bears. Beets. Battlestar Galactica.', 'Jim Halpert', 'Product Recall', 3, 21),
-('I declare bankruptcy!', 'Michael Scott', 'Money', 4, 4),
-('If I don''t have some cake soon, I might die.', 'Stanley Hudson', 'Survivor Man', 4, 7),
-('I am Beyoncé, always.', 'Michael Scott', 'Prince Family Paper', 5, 13),
-('You miss 100% of the shots you don''t take.', 'Wayne Gretzky', 'Basketball', 1, 5),
-('Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me.', 'Michael Scott', 'The Dundies', 2, 1),
-('I am running away from my responsibilities. And it feels good.', 'Michael Scott', 'Weight Loss', 5, 1),
-('Sometimes I''ll start a sentence and I don''t even know where it''s going. I just hope I find it along the way.', 'Michael Scott', 'Michael Scott Paper Company', 5, 23),
-('I''m not going to tell them about the downsizing. If a patient has cancer, you don''t tell them.', 'Michael Scott', 'Health Care', 1, 3),
-('I''ve never been a kiss up. It''s just not how I operate.', 'Jim Halpert', 'Branch Wars', 4, 6),
-('Dwight, you ignorant slut!', 'Michael Scott', 'Product Recall', 3, 21),
-('It''s Britney, b*tch.', 'Michael Scott', 'Lecture Circuit: Part 1', 5, 16),
-('The worst thing about prison was the dementors.', 'Michael Scott', 'The Convict', 3, 9),
-('That''s what she said.', 'Michael Scott', 'The Return', 3, 14),
-('I''m not superstitious, but I am a little stitious.', 'Michael Scott', 'Fun Run', 4, 1),
+// INSERT INTO quotes (quote, character, title, season, episode) VALUES
+// ('Thast&apo;s what she said.', 'Michael Scott', 'Sexual Harassment', 2, 2)
+// ('I''m not superstitious, but I am a little stitious.', 'Michael Scott', 'Fun Run', 4, 1)
+// ('Bears. Beets. Battlestar Galactica.', 'Jim Halpert', 'Product Recall', 3, 21)
+// ('I declare bankruptcy!', 'Michael Scott', 'Money', 4, 4)
+// ('If I don''t have some cake soon, I might die.', 'Stanley Hudson', 'Survivor Man', 4, 7)
+// ('I am Beyoncé, always.', 'Michael Scott', 'Prince Family Paper', 5, 13)
+// ('You miss 100% of the shots you don''t take.', 'Wayne Gretzky', 'Basketball', 1, 5)
+// ('Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me.', 'Michael Scott', 'The Dundies', 2, 1)
+// ('I am running away from my responsibilities. And it feels good.', 'Michael Scott', 'Weight Loss', 5, 1)
+// ('Sometimes I''ll start a sentence and I don''t even know where it''s going. I just hope I find it along the way.', 'Michael Scott', 'Michael Scott Paper Company', 5, 23)
+// ('I''m not going to tell them about the downsizing. If a patient has cancer, you don''t tell them.', 'Michael Scott', 'Health Care', 1, 3)
+// ('I''ve never been a kiss up. It''s just not how I operate.', 'Jim Halpert', 'Branch Wars', 4, 6)
+// ('Dwight, you ignorant slut!', 'Michael Scott', 'Product Recall', 3, 21)
+// ('It''s Britney, b*tch.', 'Michael Scott', 'Lecture Circuit: Part 1', 5, 16)
+// ('The worst thing about prison was the dementors.', 'Michael Scott', 'The Convict', 3, 9)
+// ('That''s what she said.', 'Michael Scott', 'The Return', 3, 14)
+// ('I''m not superstitious, but I am a little stitious.', 'Michael Scott', 'Fun Run', 4, 1)
